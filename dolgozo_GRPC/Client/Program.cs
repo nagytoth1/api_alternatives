abb9605176c4dd7965dc6d9bbba0fd76fdedd844
Nagy-Tóth Bence <dzkbx0@std.uni-eszterhazy.hu>
2023-06-23 07:39:22 +0200

    GRPC implementation added

---
+++ dolgozo_GRPC/Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Net.Client;
+using Server;
+using System.Configuration;
+
+string? url = ConfigurationManager.AppSettings["url"];
+if(url == null)
+{
+    Console.WriteLine("ejnyebejnye");
+    return;
+}
+GrpcChannel channel = GrpcChannel.ForAddress(url);
+/* the usage of Greeter Service:
+    Greeter.GreeterClient client = new Greeter.GreeterClient(channel);
+    HelloReply reply = await client.SayHelloAsync(new HelloRequest() { Name = "Bence" });
+    Console.WriteLine(reply.Message);
+*/
+
+
+
+Console.WriteLine("Press Enter to exit the program!");
+Console.ReadLine();
---
+++ dolgozo_GRPC/Server/Services/DolgozoService.cs
@@ -0,0 +1,68 @@
+﻿using Grpc.Core;
+
+namespace Server.Services
+{
+    public class DolgozoService:Dolgozo.DolgozoBase
+    {
+        private byte MAX_SIZE = 100;
+        private List<DolgozoModel> dolgozok;
+        private sbyte index = -1;
+        public DolgozoService()
+        {
+            dolgozok = new List<DolgozoModel>(MAX_SIZE);
+        }
+
+        public override Task<DolgozoModel> GetDolgozo(DolgozoID request, ServerCallContext context)
+        {
+            if(request.Id < 0 || request.Id >= MAX_SIZE)
+            {
+                return Task.FromResult(new DolgozoModel() { Nev = "null" });
+            }
+            return Task.FromResult(dolgozok[request.Id]);
+        }
+
+        public override Task ListDolgozok(Empty request, IServerStreamWriter<DolgozoModel> responseStream, ServerCallContext context)
+        {
+            return Task.FromResult(dolgozok);
+        }
+
+        public override Task<Response> AddDolgozo(DolgozoModel request, ServerCallContext context)
+        {
+            ++index;
+            if(index >= MAX_SIZE)
+            {
+                return Task.FromResult(new Response() { Code = 500 }); //megtelt a tömb
+            }
+            dolgozok[index] = request;
+
+            return Task.FromResult(new Response() { Code = 200 });
+        }
+
+        public override Task<Response> RemoveDolgozo(DolgozoID request, ServerCallContext context)
+        {
+            try
+            {
+                dolgozok.RemoveAt(request.Id);
+            }
+            catch (ArgumentException)
+            {
+                return Task.FromResult(new Response() { Code = 500 });
+            }
+            return Task.FromResult(new Response() { Code= 200 });
+        }
+
+        public override Task<Response> UpdateDolgozo(DolgozoUpdate request, ServerCallContext context)
+        {
+            if (request.Id.Id < 0 || request.Id.Id >= MAX_SIZE)
+            {
+                return Task.FromResult(new Response() { Code = 500 });
+            }
+            DolgozoModel found = dolgozok[request.Id.Id];
+            DolgozoModel uj = request.Dolgozo;
+            found.Nev = uj.Nev;
+            found.FotoNev = uj.FotoNev;
+            found.ReszlegId = uj.ReszlegId;
+            return Task.FromResult(new Response() { Code = 200 });
+        }
+    }
+}
